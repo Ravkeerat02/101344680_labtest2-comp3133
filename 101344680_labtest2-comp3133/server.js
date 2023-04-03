a86ae3c140c94fab33b3769a6f030185c0a57a7a
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-04-03 17:02:27 -0400

    check

--- 101344680_labtest2-comp3133/server.js
+++ 101344680_labtest2-comp3133/server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, "/101344680_labtest2-comp3133/src/index.html"));
-    res.sendFile(path.join(__dirname + "/101344680_labtest2-comp3133/src/index.html"));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(8080, () => console.log('Server started on port 8080'));
+
+app.listen(4200, () => console.log('Server started on port 4200'));
